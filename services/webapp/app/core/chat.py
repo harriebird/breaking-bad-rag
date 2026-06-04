20fc53751b7943ae5dc6de1e0a75e6ba00d96716
Paul Harriet Asiñero <20557045+harriebird@users.noreply.github.com>
2026-06-04 16:24:47 +0800

    Connect chat interface to vector DB and LLM (#3)
    * Connected chat interface to vector DB and LLM
    
    * Disable showing of references if the answer is I don't know

---
+++ services/webapp/app/core/chat.py
@@ -0,0 +1,53 @@
+from ollama import Client
+from sqlmodel import Session, select
+
+from core import config
+from models import engine, Paragraph
+
+_llm_client = Client(host=config.OLLAMA_HOST)
+
+def do_search(query):
+    if query:
+        with Session(engine) as session:
+            response = _llm_client.embed(
+                model=config.OLLAMA_EMBEDDING_MODEL,
+                input=query,
+            )
+
+            embed_query = response.embeddings[0]
+
+            paragraphs = session.exec(select(Paragraph).order_by(Paragraph.embedding.l2_distance(embed_query)).limit(5))
+            contexts = [paragraph for paragraph in paragraphs]
+            return contexts
+
+def do_prompt(query, contexts):
+    template = f"""
+Use only the provided contexts to answer the question.
+Make your answer concise in one paragraph.
+If you don't know the answer, just say "I don't know. Ask something about Breaking Bad.".
+
+Contexts:
+{"\n".join(context.text for context in contexts)}
+
+Question:
+{query}
+"""
+    response = _llm_client.generate(
+        model=config.OLLAMA_LANGUAGE_MODEL,
+        prompt=template,
+        stream=False
+    )
+    return response.response
+
+def do_chat(text):
+    results = do_search(text)
+    response = do_prompt(text, results)
+    references = "\n\nReferences:"
+    for result in results:
+        references += f"\nS{result.season:02d}E{result.episode:02d}: {result.title}\n Link: {result.plot_url}\n"
+
+    if not "don't know" in response:
+        response += references
+    
+    return response
+
