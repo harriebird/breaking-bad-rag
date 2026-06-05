784b848625169d5b00f2ab8945fdd5f92b508162
Paul Harriet Asiñero <20557045+harriebird@users.noreply.github.com>
2026-06-05 14:00:42 +0800

    Added /help chat command

--- services/webapp/app/main.py
+++ services/webapp/app/main.py
@@ -4,11 +4,16 @@ from sqlmodel import SQLModel
 from core import embed, chat
 
 def receive_message(message, history):
+    if "/help" in message:
+        return "Welcome to Breaking Bad RAG! Here are some commands that will help:\n" \
+               "`/help` - displays this help message.\n" \
+               "`/start-embedding` - starts embedding the Breaking Bad knowledge base and stores it to a vector DB.\n" \
+               "`/clear-embedding` - clears the existing embedding records in the vector DB."
 
     if "/start-embedding" in message:
         count = embed.count_db_content()
         if count > 0:
-            return f"Can't start embedding. There are {count} existing records. \n" \
+            return f"Can't start embedding. There are {count} existing records.\n" \
                    "Try `/clear-embedding` first to clear all the existing records."
 
         success = embed.embed()
@@ -23,7 +28,7 @@ def receive_message(message, history):
 def clear_metadata():
     SQLModel.metadata.clear()
 
-with gr.ChatInterface(fn=receive_message, examples=["/start-embedding"], title="Breaking Bad RAG") as chat_app:
+with gr.ChatInterface(fn=receive_message, examples=["/help"], title="Breaking Bad RAG") as chat_app:
     chat_app.load(fn=clear_metadata)
 
 chat_app.launch()
