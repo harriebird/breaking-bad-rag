61364d3657c0ed8608681972aa5e3e06703c9b85
Paul Harriet Asiñero <20557045+harriebird@users.noreply.github.com>
2026-06-21 16:36:48 +0800

    Added Ollama installer script

---
+++ services/ollama/setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+case $1 in
+  update)
+    curl -fsSL https://ollama.com/install.sh | sh
+    ;;
+  install)
+    ./setup.sh update
+    sudo cp ollama.service /etc/systemd/system/
+    sudo systemctl daemon-reload
+    sudo systemctl stop ollama
+    sudo systemctl start ollama
+    ./setup.sh pullmodel
+    ;;
+  pullmodel)
+    ollama pull hf.co/unsloth/embeddinggemma-300m-GGUF:Q4_0
+    ollama pull hf.co/unsloth/gemma-3-1b-it-GGUF:Q4_K_M
+    ;;
+  uninstall)
+    ollama rm hf.co/unsloth/embeddinggemma-300m-GGUF:Q4_0
+    ollama rm hf.co/unsloth/gemma-3-1b-it-GGUF:Q4_K_M
+    sudo systemctl stop ollama
+    sudo systemctl disable ollama
+    sudo rm /etc/systemd/system/ollama.service
+    sudo rm -r $(which ollama | tr 'bin' 'lib')
+    sudo rm $(which ollama)
+    ;;
+#  *)
+#    ;;
+esac
